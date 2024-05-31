0d226fae9fecfa5a3dfef82c8525b56ff8ba9312
Gamba <dmitriyi.nikitiuk@gmail.com>
2024-06-01 02:00:27 +0300

    v1

--- hv06_addressbook.py
+++ hv06_addressbook.py
@@ -38,7 +38,7 @@ class Record:
     def edit_phone(self, old_phone_number, new_phone_number):
         for phone in self.phones:
             if phone.value == old_phone_number:
-                if phone.validate(new_phone_number):
+                if Phone.validate(new_phone_number):
                     phone.value = new_phone_number
                 else:
                     raise ValueError("New phone number must be 10 digits.")
@@ -63,3 +63,36 @@ class AddressBook(UserDict):
     def delete(self, name):
         if name in self.data:
             del self.data[name]
+
+# Створення нової адресної книги
+# book = AddressBook()
+
+    # Створення запису для John
+# john_record = Record("John")
+# john_record.add_phone("1234567890")
+# john_record.add_phone("5555555555")
+
+    # Додавання запису John до адресної книги
+# book.add_record(john_record)
+#
+#     # Створення та додавання нового запису для Jane
+# jane_record = Record("Jane")
+# jane_record.add_phone("9876543210")
+# book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+# for name, record in book.data.items():
+#     print(record)
+#
+#     # Знаходження та редагування телефону для John
+# john = book.find("John")
+# john.edit_phone("1234567890", "11122099999999999999923333")
+#
+# print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+#
+#     # Пошук конкретного телефону у записі John
+# found_phone = john.find_phone("5555555555")
+# print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+#
+#     # Видалення запису Jane
+# book.delete("Jane")
